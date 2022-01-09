8dcd124bf9ed60ff11325f648f65be0c04f21dda
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2022-01-08 22:15:21 -0800

    landing page template and restructuring of files

---
+++ yesteryear/assets/javascript/app.js
@@ -0,0 +1,52 @@
+"use strict";
+var nav = $("nav");
+var navHeight = nav.outerHeight();
+
+// $(".navbar-toggler").on("click", function() {
+//     if (! $('#mainNav').hasClass('navbar-reduce')) {
+//         $("#mainNav").addClass('navbar-reduce');
+//     }
+// });
+
+// Preloader
+$(window).on("load", function() {
+    if ($('#preloader').length) {
+        $('#preloader').delay(100).fadeOut('slow', function() {
+            $(this).remove();
+        })
+    }
+})
+
+// Back to Top button
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 90) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+
+// Active scrollspy
+$('body').scrollspy({
+    target: '#mainNav',
+    offset: navHeight
+});
+
+// Navbar Reduce
+$(window).trigger('scroll');
+$(window).on('scroll', function() {
+    let pixels = 50;
+    let top = 1200;
+    if($(window).scrollTop() > pixels) {
+        $('.navbar-expand-md').addClass('navbar-reduce');
+        $('.navbar-expand-md').removeClass('navbar-trans');
+    } else {
+        $('.navbar-expand-md').addClass('navbar-trans');
+        $('.navbar-expand-md').removeClass('navbar-reduce');
+    }
+    if ($(window).scrollTop() > top) {
+        $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+    } else {
+        $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+    }
+});
